1096fa8dc33fd2e35716e6d5bc204d3e9821ae6c
noraworld <mail@noraworld.com>
2021-04-29 15:00:04 +0900

    Add ability to detect ad block (WIP)

---
+++ assets/js/adblock_detection.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const adSpaces = document.querySelectorAll('.ad');
+//   const adFrames = document.querySelectorAll('.ad iframe');
+//   const element = document.createElement('div');
+
+//   if (adFrames.length === 0) {
+//     for (var i = 0; i < adSpaces.length; i++) {
+//       console.log(adSpaces[i]);
+//       adSpaces[i].appendChild(element);
+//     }
+//   }
+// });
